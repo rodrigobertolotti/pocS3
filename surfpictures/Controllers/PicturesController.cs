5702a1906a000c1ee0b10cfb5f0f849458d77dc5
Rodrigo Bertolotti <rodrigobertolotti1995@gmail.com>
2021-12-17 16:45:05 -0300

    Initial commit

---
+++ surfpictures/Controllers/PicturesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace surfpictures.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PicturesController : ControllerBase
+    {
+        private readonly IAmazonS3 amazonS3;
+        public PicturesController(IAmazonS3 amazonS3)
+        {
+            this.amazonS3 = amazonS3;
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadImages([FromForm] List<IFormFile> files)
+        {
+            for (int i=0; i < files.Count; i++)
+            {
+                var putRequest = new PutObjectRequest()
+                {
+                    BucketName = "surfuruguay",
+                    Key = files[i].FileName,
+                    InputStream = files[i].OpenReadStream(),
+                    ContentType = files[i].ContentType
+                };
+                var result = await this.amazonS3.PutObjectAsync(putRequest);
+                if (result.HttpStatusCode != System.Net.HttpStatusCode.OK)
+                {
+                    return BadRequest(result);
+                }
+            }
+            return Ok("Upload successfull."); 
+        }
+    }
+}
